74f93af997a19d708ea819cbfb08b731b094b8b4
kpi-v <126921655+kpi-v@users.noreply.github.com>
2025-06-06 02:20:07 +0200

    Delete js/buttons_profile.js

--- js/buttons_profile.js
+++
@@ -1,7 +0,0 @@
-function save_btn(){
-    showAlert("<strong>Зауваження:</strong> програмувати функціональність сайту не потрібно (!)");
-}
-
-function cancle_btn(){
-    showAlert("<strong>Зауваження:</strong> програмувати функціональність сайту не потрібно (!)");
-}
